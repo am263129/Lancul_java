f003bae48b9050e2e42ed613eec7ec573d7e3c15
Viking <fashioncstar2019@outlook.com>
2020-03-14 10:25:37 +0300

    step 5-3
    restaurant adapter

--- sample/src/main/java/com/arabian/lancul/UI/Adapter/RestaurantAdapter.java
+++ sample/src/main/java/com/arabian/lancul/UI/Adapter/RestaurantAdapter.java
@@ -1,14 +1,20 @@
 package com.arabian.lancul.UI.Adapter;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
 import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.arabian.lancul.MainActivity;
 import com.arabian.lancul.R;
@@ -17,55 +23,135 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
-public class RestaurantAdapter extends ArrayAdapter<Restaurant> implements Filterable {
+public class RestaurantAdapter extends RecyclerView.Adapter<RestaurantAdapter.PlanetHolder> implements Filterable {
 
-
-    ArrayList<Restaurant> restaurants = new ArrayList<Restaurant>();
-    Context mcontext;
-    ImageView heart;
+    private Context context;
+    private ArrayList<Restaurant> restaurants;
     boolean like = false;
-    public RestaurantAdapter(Context context, int textViewResourceId, ArrayList<Restaurant> objects) {
-        super(context, textViewResourceId, objects);
-        restaurants = objects;
-        mcontext = context;
-    }
-    @Override
-    public int getCount() {
-        return super.getCount();
+
+    public RestaurantAdapter(Context context, ArrayList<Restaurant> restaurants) {
+        this.context = context;
+        this.restaurants = restaurants;
     }
 
+    @NonNull
     @Override
-    public View getView(final int position, View convertView, ViewGroup parent) {
+    public PlanetHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_restaurant, parent, false);
+        return new PlanetHolder(view);
+    }
 
-        View v = convertView;
-        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        v = inflater.inflate(R.layout.item_restaurant, null);
-
-        ImageView restaurant_photo;
+    @Override
+    public void onBindViewHolder(@NonNull PlanetHolder holder, int position) {
         Restaurant restaurant = restaurants.get(position);
-        TextView name = v.findViewById(R.id.label_restaurant);
-        TextView location = v.findViewById(R.id.location);
-        RatingBar ratingBar = v.findViewById(R.id.ratingBar);
-        restaurant_photo = v.findViewById(R.id.item_photo);
-        heart = v.findViewById(R.id.btn_heart);
-        name.setText(restaurant.getName());
-        location.setText(restaurant.getLocation());
-        ratingBar.setRating(restaurant.getRating());
-
-        Glide.with(MainActivity.getInstance()).load(restaurant.getPhoto()).apply(new RequestOptions().override(200, 400)).into(restaurant_photo);
-
-        heart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                like = !like;
-                if (like)
-                    heart.setBackgroundResource(R.drawable.full_heart);
-                else
-                    heart.setBackgroundResource(R.drawable.empty_heart);
-            }
-        });
-        return v;
-
+        holder.setDetails(restaurant);
     }
+
+    @Override
+    public int getItemCount() {
+        return restaurants.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return null;
+    }
+
+    class PlanetHolder extends RecyclerView.ViewHolder {
+
+        private TextView name, location;
+        private RatingBar ratingBar;
+        private ImageView restaurant_photo, heart;
+        private CardView card;
+
+
+        PlanetHolder(View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.label_restaurant);
+            location = itemView.findViewById(R.id.location);
+            ratingBar = itemView.findViewById(R.id.ratingBar);
+            restaurant_photo = itemView.findViewById(R.id.item_photo);
+            heart = itemView.findViewById(R.id.btn_heart);
+            card = itemView.findViewById(R.id.card_view);
+        }
+
+        void setDetails(Restaurant restaurant) {
+            name.setText(restaurant.getName());
+            location.setText(restaurant.getLocation());
+            ratingBar.setRating(restaurant.getRating());
+
+            Glide.with(MainActivity.getInstance()).load(restaurant.getPhoto()).apply(new RequestOptions().override(200, 400)).into(restaurant_photo);
+
+            card.setCardBackgroundColor(Color.parseColor("#E6E6E6"));
+            card.setMaxCardElevation((float) 0.0);
+            card.setRadius((float) 50);
+
+            heart.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    like = !like;
+                    if (like)
+                        heart.setBackgroundResource(R.drawable.full_heart);
+                    else
+                        heart.setBackgroundResource(R.drawable.empty_heart);
+                }
+            });
+            }
+    }
+
+
+
+//    ArrayList<Restaurant> restaurants = new ArrayList<Restaurant>();
+//    Context mcontext;
+//    ImageView heart;
+//    boolean like = false;
+//    public RestaurantAdapter(Context context, int textViewResourceId, ArrayList<Restaurant> objects) {
+//        super(context, textViewResourceId, objects);
+//        restaurants = objects;
+//        mcontext = context;
+//    }
+//    @Override
+//    public int getCount() {
+//        return super.getCount();
+//    }
+//
+//    @Override
+//    public View getView(final int position, View convertView, ViewGroup parent) {
+//
+//        View v = convertView;
+//        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//        v = inflater.inflate(R.layout.item_restaurant, null);
+//
+//        ImageView restaurant_photo;
+//        Restaurant restaurant = restaurants.get(position);
+//        TextView name = v.findViewById(R.id.label_restaurant);
+//        TextView location = v.findViewById(R.id.location);
+//        RatingBar ratingBar = v.findViewById(R.id.ratingBar);
+//        restaurant_photo = v.findViewById(R.id.item_photo);
+//        heart = v.findViewById(R.id.btn_heart);
+//        name.setText(restaurant.getName());
+//        location.setText(restaurant.getLocation());
+//        ratingBar.setRating(restaurant.getRating());
+//
+//        Glide.with(MainActivity.getInstance()).load(restaurant.getPhoto()).apply(new RequestOptions().override(200, 400)).into(restaurant_photo);
+//        CardView card = v.findViewById(R.id.card_view);
+//        card.setCardBackgroundColor(Color.parseColor("#E6E6E6"));
+//        card.setMaxCardElevation((float) 0.0);
+//        card.setRadius((float) 50);
+//
+//        heart.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                like = !like;
+//                if (like)
+//                    heart.setBackgroundResource(R.drawable.full_heart);
+//                else
+//                    heart.setBackgroundResource(R.drawable.empty_heart);
+//            }
+//        });
+//        return v;
+//
+//    }
 }
--- sample/src/main/java/com/arabian/lancul/UI/Fragment/RetaurantFragment.java
+++ sample/src/main/java/com/arabian/lancul/UI/Fragment/RetaurantFragment.java
@@ -8,7 +8,10 @@ import android.widget.ListView;
 
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.arabian.lancul.MainActivity;
 import com.arabian.lancul.R;
 import com.arabian.lancul.UI.Adapter.RestaurantAdapter;
 import com.arabian.lancul.UI.Object.Restaurant;
@@ -18,7 +21,7 @@ import java.util.ArrayList;
 public class RetaurantFragment extends Fragment {
 
     View view;
-    ListView list;
+    RecyclerView list;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -32,8 +35,8 @@ public class RetaurantFragment extends Fragment {
         restaurants.add(new Restaurant("Launch of Region Commander | إطلاق قائد المنطقة","Dubai","https://firebasestorage.googleapis.com/v0/b/elsa-project-12a47.appspot.com/o/restaurant_3.jpg?alt=media&token=42f827bc-3008-4167-86c1-33842b723e5a",(float)4));
         restaurants.add(new Restaurant("Huge Tiny |  ضخمة صغيرة","Dubai","https://firebasestorage.googleapis.com/v0/b/elsa-project-12a47.appspot.com/o/restaurant_4.jpg?alt=media&token=bad33dbf-3f56-4696-ab00-e6f6e8396c6b",(float)4.5));
 
-        RestaurantAdapter adapter  = new RestaurantAdapter(getContext(),R.layout.item_restaurant, restaurants);
-
+        RestaurantAdapter adapter  = new RestaurantAdapter(getContext(), restaurants);
+        list.setLayoutManager(new LinearLayoutManager(MainActivity.getInstance()));
         list.setAdapter(adapter);
         return view;
     }
